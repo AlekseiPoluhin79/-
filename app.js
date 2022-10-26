f4fcb8112e107f417e8730f1d241c2824924c807
Aleksei Poluhin <Poluhin1979@outlook.com>
2022-10-26 17:50:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const cols = document.querySelectorAll('.col')
+
+// функция которая обновляет страницу при нажатии пробела
+document.addEventListener('keydown', (event) =>{
+    event.preventDefault()
+    if (event.code.toLowerCase() ==="space"){
+        setRendomColors()
+    }
+})
+
+//функция открытия или закрытия замка
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type === 'lock'){
+        const node = event.target.tagName.toLowerCase() === 'i' ? event.target : event.target.children[0]     
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+})
+
+
+// функция генерации случайного цвета
+function generateRandomColor(){
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for( let i = 0; i < 6; i++ ) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+   return '#' + color
+}
+
+
+// функция отображения случайного цвета 
+function setRendomColors(){
+    cols.forEach(col =>{
+       const text =  col.querySelector('h2')
+       const button =  col.querySelector('button')
+       const color = generateRandomColor()  //вместо функции generateRandomColor() можно использовать библиотеку chroma.js
+       
+       text.textContent = color
+       col.style.background = color
+
+     setTextColor(text, color)
+     setTextColor(button, color)
+    }) 
+}
+
+// определяет цвет оттенка и изменяем его
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()   // применяем библиотеку chroma.js для отображения оттенков
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRendomColors();
